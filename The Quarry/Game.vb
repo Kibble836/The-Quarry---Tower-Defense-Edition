4c1dd02dbb69166b9fcf4e3615d7997c887a83a1
Kibble836 <30538587+Kibble836@users.noreply.github.com>
2018-06-02 16:33:37 -0400

    Began frmLoad

---
+++ The Quarry/Game.vb
@@ -0,0 +1,22 @@
+﻿Public Class Game
+
+    'Private Variables
+
+
+
+    Private Sub Game_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Initializing form
+        Me.ClientSize = New Size(1000, 1000)
+        Me.Name = "The Quarry"
+
+        'Create Objects
+        'Player
+        Dim Player As New PictureBox
+        With Player
+            .Size = New Size(32, 32)
+            .Name = "Player"
+
+        End With
+
+    End Sub
+End Class
---
+++ The Quarry/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("The Quarry")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("The Quarry")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6e40c307-6dbb-4e41-8dc6-3dee6d84a120")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>

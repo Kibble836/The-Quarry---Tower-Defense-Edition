8d5ba82882f7ea3c9d38aab84734646acc7714ad
Kibble836 <30538587+Kibble836@users.noreply.github.com>
2018-06-02 17:23:19 -0400

    Completed player & Crystal object declarations

--- The Quarry/Game.vb
+++ The Quarry/Game.vb
@@ -5,8 +5,12 @@
 
 
     Private Sub Game_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Constants
+        Const xMapSize As Integer = 1024
+        Const yMapSize As Integer = 1024
+
         'Initializing form
-        Me.ClientSize = New Size(1024, 1024)
+        Me.ClientSize = New Size(xMapSize, yMapSize)
         Me.Name = "The Quarry"
 
         'Create Objects
@@ -15,17 +19,21 @@
         With Player
             .Size = New Size(32, 32)
             .Name = "Player"
-
+            .Visible = True
+            .BackColor = Color.Red
+            .Location = New Point(xMapSize / 2 - 16, yMapSize / 2 + 48)
+            Controls.Add(Player)
         End With
         'Crystal
         Dim Crystal As New PictureBox
         With Crystal
             .Size = New Size(64, 32)
             .Name = "Crystal"
-            .Location = New Point(512 - 32, 512 - 16)
-            .BackColor = Color.Black
+            .Location = New Point(xMapSize / 2 - 32, yMapSize / 2 - 16)
+            .Image = My.Resources.Crystal
             .Visible = True
+            Controls.Add(Crystal)
         End With
-        Controls.Add(Crystal)
+
     End Sub
 End Class

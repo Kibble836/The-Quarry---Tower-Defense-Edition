118ced8a8196bf481e09633b85d42caa4ae65a3c
Kibble836 <30538587+Kibble836@users.noreply.github.com>
2018-06-02 16:49:16 -0400

    Crystal

--- The Quarry/Game.vb
+++ The Quarry/Game.vb
@@ -6,7 +6,7 @@
 
     Private Sub Game_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Initializing form
-        Me.ClientSize = New Size(1000, 1000)
+        Me.ClientSize = New Size(1024, 1024)
         Me.Name = "The Quarry"
 
         'Create Objects
@@ -17,6 +17,15 @@
             .Name = "Player"
 
         End With
-
+        'Crystal
+        Dim Crystal As New PictureBox
+        With Crystal
+            .Size = New Size(64, 32)
+            .Name = "Crystal"
+            .Location = New Point(512 - 32, 512 - 16)
+            .BackColor = Color.Black
+            .Visible = True
+        End With
+        Controls.Add(Crystal)
     End Sub
 End Class
